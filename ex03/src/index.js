13a926ba7620e02ddfc6295c0c24f0beb270432b
Safijaz <79872266+Safijaz@users.noreply.github.com>
2021-03-19 02:42:21 +0100

    Add files via upload

---
+++ ex03/src/index.js
@@ -0,0 +1,13 @@
+ var myPetsArray= ["Dog","Cat"];
+ // End of temps arrey
+ 
+ function myArreyFunction(myPets){
+     var myNewPets = [...myPets];
+     
+     // only change code below this line 
+    
+     // only change code above this line
+     return ;
+ }
+ console.log (myArreyFunction());
+ module.exports = myArreyFunction;
